2b7602022fedab61900811ea1157aa3c035c8356
irinaoloinic <133090430+irinaoloinic@users.noreply.github.com>
2023-10-05 09:20:40 +0200

    added axios and the Weatherapp component

--- src/Search.js
+++ src/Search.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 function Search(){
   return(
@@ -7,5 +8,15 @@ function Search(){
   <input type="button"> </input>
 </form>
   )
+function showTemperature(response){
+
+  
+}
+
+  let apiKey="37f6a94ba46fa4c11df96b5390cdt21o";
+  let apiURL = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+
+
 }
 export default Search;
