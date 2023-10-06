a17a5c9346c3e3b556c23fe08a1e998ac675856c
irinaoloinic <133090430+irinaoloinic@users.noreply.github.com>
2023-10-06 14:49:55 +0200

    migrated the weather app to react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Weather from './Weather';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>Weather forecast</h1>
+    
+      <Weather />
       </header>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Weather.css";
 
-
 function Weather(){
   return(
     <div className="container">
